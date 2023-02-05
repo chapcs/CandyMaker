4e6dbad61684429de553b08e3f5147831af50795
cozz-py <bencozzens13@gmail.com>
2023-02-05 07:19:25 -0500

    Classes added, still need methods

---
+++ CandyMaker/Maker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyMaker
+{
+    public class Maker
+    {
+        TemperatureChecker tcheck = new TemperatureChecker();
+        int t = 165;// temp in C
+        
+        public int CheckNougatTemperature()
+        {
+            Console.WriteLine("Nougat temperature: " + t + " C");
+            return t;
+        }
+        public void CheckAirSystem()
+        {
+                Console.WriteLine("Checking Air System . . .");
+                if (tcheck.air == true)
+                {
+                    Console.WriteLine("Air Present in System");
+                }
+        }
+    }
+}
--- CandyMaker/Program.cs
+++ CandyMaker/Program.cs
@@ -1,5 +1,47 @@
-﻿internal class Program
+﻿using CandyMaker;
+using System.Security.Cryptography.X509Certificates;
+
+internal class TemperatureChecker
 {
+    public bool air = false;// air system is not checked yet
+    Maker maker = new Maker();
+    /// <summary>
+    /// If the nougat temperature exceeds 160C it's too hot
+    /// </summary>
+    public static bool IsNougatTooHot()
+    {
+        int temp = maker.CheckNougatTemperature();
+        if (temp > 160)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    /// <summary>
+    /// Perform the Candy Isolation Cooling System vent procedure
+    /// </summary>
+    public void DoCICSVentProcedure()
+    {
+        TurbineController turbines = new TurbineController();
+        turbines.CloseTripValve(2);
+        IsolationCoolingSystem.Fill();
+        IsolationCoolingSystem.Vent();
+        maker.CheckAirSystem();
+    }
+    /// <summary>
+    /// This code runs every 3 minutes to check the temperature.
+    /// If it exceeds 160c we need to vent the cooling system.
+    /// </summary>
+    public void ThreeMinuteCheck()
+    {
+        if (IsNougatTooHot() == true)
+        {
+            DoCICSVentProcedure();
+        }
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("hello");
